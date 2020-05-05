5e9f08e9ee5ceeaf08e25ba095773e5fcc924d9d
Sean Galantzan <sean.galantzan@cern.ch>
2020-05-05 11:20:46 +0300

    Trying to make second re-run of DHNLAlgorithm work with BE.

--- source/DHNLAlgorithm/data/full_config_DHNLAlgorithm.py
+++ source/DHNLAlgorithm/data/full_config_DHNLAlgorithm.py
@@ -68,7 +68,7 @@ basicEventSelectionDict = {
     "m_storeTrigDecisions"        : True,
     "m_storePassL1"               : True,
     "m_storeTrigKeys"             : True,
-    "m_applyTriggerCut"           : False,
+    "m_applyTriggerCut"           : True,
     "m_doPUreweighting"           : False if o.noPRW else args.is_MC,
     "m_PRWFileNames"              : PRW,
     "m_lumiCalcFileNames"         : lumicalcs,
@@ -98,7 +98,7 @@ DHNLFilterDict = {
     "m_inMuContainerName"       : "Muons",
     "m_inElContainerName"       : "Electrons",
     "m_vertexContainerName"     : "PrimaryVertices",
-    "m_applyFilterCut"          : False,
+    "m_applyFilterCut"          : True,
     # "m_secondaryVertexContainerName" : "VrtSecInclusive_SecondaryVertices",
 
     #----------------------- Selections ----------------------------#
