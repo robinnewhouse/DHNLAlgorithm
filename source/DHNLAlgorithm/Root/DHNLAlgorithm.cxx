4d72ddcad059f0ced721be6ea77b534bc7bd7220
Sean Galantzan <sean.galantzan@cern.ch>
2020-06-08 10:28:35 +0300

    Fix bug

--- source/DHNLAlgorithm/Root/DHNLAlgorithm.cxx
+++ source/DHNLAlgorithm/Root/DHNLAlgorithm.cxx
@@ -140,7 +140,7 @@ EL::StatusCode DHNLAlgorithm::execute() {
                 msInnerMatchDOF = -1;
             muon->auxdecor<int>("msDOF") = msInnerMatchDOF;
 
-            if (muon->primaryTrackParticle()->isAvailable<unsigned long>("patternRecoInfo") ){
+            if (muon->primaryTrackParticle()->isAvailable<unsigned long>("patternRecoInfo") )
                 muon->auxdecor<bool>("isLRT") = muon->primaryTrackParticle()->patternRecoInfo().test(xAOD::SiSpacePointsSeedMaker_LargeD0);
         }
     }
