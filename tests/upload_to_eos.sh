a4bfce89a99ceed2477fd44cdb8dd0b32c1f8dc1
Colm Sam <colm.sam@cern.ch>
2020-11-05 13:59:27 -0800

    fixed exclude statements in rsync command

--- tests/upload_to_eos.sh
+++ tests/upload_to_eos.sh
@@ -2,6 +2,7 @@ echo "Uploading test run of current commit to eos"
 last_commit=$(git rev-parse --verify --short HEAD)
 uploaded_dir="tests_$last_commit"
 rsync -av -e ssh \
---exclude='*.sh' --exclude='*.py'--exclude='*.txt' --exclude='*.swp' --exclude='*.pyc'  \
+--exclude "*.sh" --exclude "*.py" --exclude "*.txt" --exclude "*.swp" --exclude "*.pyc"  \
 ../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
+
 
