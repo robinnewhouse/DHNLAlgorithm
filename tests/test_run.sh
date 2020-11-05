88144355254402934887533703e2a4afa08d9aa5
Colm Sam <colm.sam@cern.ch>
2020-11-05 08:30:57 -0800

    moved run script to tests dir and attempt 1 to upload tests dir to eos

---
+++ tests/test_run.sh
@@ -0,0 +1,13 @@
+echo "Testing DHNLAlgorithm"
+pwd
+ls
+# source build/*/setup.sh
+xAH_run.py \
+--config $WorkDir_DIR/data/DHNLAlgorithm/config_DHNLAlgorithm.py \
+--inputList \
+--files files.txt \
+--submitDir testRun \
+--force \
+--isMC \
+--nevents=10000 \
+direct
