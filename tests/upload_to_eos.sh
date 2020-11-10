922199730035e6ef2f3b2c17dcb2d8caf9892ab5
Colm Sam <colm.sam@cern.ch>
2020-11-10 14:16:07 -0800

    trying xrdcp protocol to copy over files to eos

--- tests/upload_to_eos.sh
+++ tests/upload_to_eos.sh
@@ -1,12 +1,8 @@
 echo "Uploading test run of current commit to eos"
 last_commit=$(git rev-parse --verify --short HEAD)
 uploaded_dir="tests_$last_commit"
-pwd
-ls
-cp -r ../tests/  /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
-scp -r ../tests/  /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
-rsync -av -e ssh \
---exclude "*.sh" --exclude "*.py" --exclude "*.txt" --exclude "*.swp" --exclude "*.pyc"  \
-../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
-
-
+#eos root://eosuser.cern.ch
+#rsync -av -e ssh \
+#--exclude "*.sh" --exclude "*.py" --exclude "*.txt" --exclude "*.swp" --exclude "*.pyc"  \
+#../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
+xrdcp ../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
