af71aa37c4e078b855f6c940d874522a2b601635
Colm Sam <colm.sam@cern.ch>
2020-11-10 12:50:32 -0800

    eos path not found in rsync, debugging with pwd and ls

--- tests/upload_to_eos.sh
+++ tests/upload_to_eos.sh
@@ -1,6 +1,10 @@
 echo "Uploading test run of current commit to eos"
 last_commit=$(git rev-parse --verify --short HEAD)
 uploaded_dir="tests_$last_commit"
+pwd
+ls
+cp -r ../tests/  /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
+scp -r ../tests/  /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
 rsync -av -e ssh \
 --exclude "*.sh" --exclude "*.py" --exclude "*.txt" --exclude "*.swp" --exclude "*.pyc"  \
 ../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
