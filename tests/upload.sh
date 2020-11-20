4cf84a55572b281e409fc9c3be90a31d6bfcc463
Colm Sam <colm.sam@cern.ch>
2020-11-19 20:33:36 -0800

    changed the upload, it didnt have git, so now using root-conda

--- tests/upload.sh
+++ tests/upload.sh
@@ -8,6 +8,6 @@ rm *.pw?
 #make a copy to upload to eos, labelled by git commit hash
 last_commit=$(git rev-parse --verify --short HEAD)
 upload_dir="tests_$last_commit"
-cp ../tests ../$upload_dir
+cp -r ../tests ../$upload_dir
 #upload to eos
 xrdcp -r ../$upload_dir root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/
