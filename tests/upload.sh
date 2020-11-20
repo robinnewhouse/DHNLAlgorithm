e911aa17c27de43805c4ecc3eef2828a00e9160a
Colm Sam <colm.sam@cern.ch>
2020-11-19 20:08:33 -0800

    fixed naming problems, relative paths in upload.sh

--- tests/upload.sh
+++ tests/upload.sh
@@ -1,12 +1,14 @@
 #this script uploads results and files from ci tests and uploads them to eos
+cd tests
 echo "Uploading test run of current commit to eos"
 #removing unnecessary files 
 rm *.py
 rm *.sh
 rm *.pyc
+rm *.pw?
 #make a copy to upload to eos, labelled by git commit hash
 last_commit=$(git rev-parse --verify --short HEAD)
 upload_dir="tests_$last_commit"
 cp ../tests ../$upload_dir
 #upload to eos
-xrdcp -r ../tests/ root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/$upload_dir
+xrdcp -r ../$upload_dir root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/
