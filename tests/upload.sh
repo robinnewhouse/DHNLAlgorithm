8db29bb4154aa11c9b29f4d3d85c832219e9c767
Colm Sam <colm.sam@cern.ch>
2020-11-19 19:10:19 -0800

    renamed upload_to_eos to upload.sh, added more features to upload.sh, removed copies of run.sh in scripts, added stuff to gitignore

---
+++ tests/upload.sh
@@ -0,0 +1,12 @@
+#this script uploads results and files from ci tests and uploads them to eos
+echo "Uploading test run of current commit to eos"
+#removing unnecessary files 
+rm *.py
+rm *.sh
+rm *.pyc
+#make a copy to upload to eos, labelled by git commit hash
+last_commit=$(git rev-parse --verify --short HEAD)
+upload_dir="tests_$last_commit"
+cp ../tests ../$upload_dir
+#upload to eos
+xrdcp -r ../tests/ root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/$upload_dir
--- tests/upload_to_eos.sh
+++
@@ -1,9 +0,0 @@
-echo "Uploading test run of current commit to eos"
-last_commit=$(git rev-parse --verify --short HEAD)
-uploaded_dir="tests_$last_commit"
-#eos root://eosuser.cern.ch
-#rsync -av -e ssh \
-#--exclude "*.sh" --exclude "*.py" --exclude "*.txt" --exclude "*.swp" --exclude "*.pyc"  \
-#../tests/ /eos/user/d/dhnl/ci-test-runs/tests_$last_commit
-xrdcp -r ../tests/ root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/
-xrdcp ../tests/ root://eosuser.cern.ch//eos/user/d/dhnl/ci-test-runs/tests_$last_commit
