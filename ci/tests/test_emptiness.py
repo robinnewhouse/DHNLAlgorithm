f0a9716d8d7895ce291ceee3dc5749d879b99cb2
Colm Sam <colm.sam@cern.ch>
2020-12-15 16:37:49 -0800

    forgot to remove relative importing usage

--- ci/tests/test_emptiness.py
+++ ci/tests/test_emptiness.py
@@ -8,7 +8,6 @@ def test_emptiness(hists):
     #obtain list of variables that were histogrammed
     variables = [key.GetName() for key in f.GetListOfKeys()]
     #find (if any) the histograms that are empty
-    variables = ranges.keys()
     empty_variables = [variable for variable in variables if sum(f.Get(variable))<=0]
     if len(empty_variables)!=0:
         raise ValueError("The following histograms were found to be empty:\n{0}".format(empty_variables)) #fail
